ac64ce367d79813cd9d6d579a4fe4cce018ae31d
cplopez4 <cplopez4@uc.cl>
2014-11-06 02:48:10 -0300

    Spree Clean App Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeClean::Application.config.secret_key_base = '6033f436033ec1c6e41de1ee862f0acbf9fbf76ae3ece40cfabad800e5b98fa0b07971eb06570ed1ab0f58e1e3d83deae62946dee6d421afaa6468ab65280f5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpreeClean::Application.config.session_store :cookie_store, key: '_spree-clean_session'
